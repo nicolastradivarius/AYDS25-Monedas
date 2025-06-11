d22a5b7aebd36645802016d7f6ccc2ca6d6a9d0c
Kevin Schweitzer <kevin.schweitzer.external@sonova.com>
2025-06-11 10:04:13 -0300

    Add mockk lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.gson)
+    implementation(libs.mockk)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
